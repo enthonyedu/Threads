2600ed510bc14e7fb14909185003085efe79a002
enthony <enthonyedu@gmail.com>
2022-03-29 22:30:24 -0300

    feat: criado a classe caminhao e seus metodos

--- src/main/java/br/com/lds/Main.java
+++ src/main/java/br/com/lds/Main.java
@@ -1,5 +1,7 @@
 package br.com.lds;
 
+import br.com.lds.models.Caminhao;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -8,6 +10,15 @@ public class Main {
 	}
 
 	private void start() {
+
+		Caminhao primeiroCaminhao = new Caminhao("TiburssiusLog", 1);
+		Caminhao segundoCaminhao = new Caminhao("AroldusLog", 2);
+
+		Thread threadPrimeiroCaminhao = new Thread(primeiroCaminhao);
+		Thread threadSegundoCaminhao = new Thread(segundoCaminhao);
+
+		threadPrimeiroCaminhao.start();
+		threadSegundoCaminhao.start();
 
 	}
 
